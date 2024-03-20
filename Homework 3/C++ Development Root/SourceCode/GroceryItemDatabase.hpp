af39d69d56e87c08e8098670839619663f94abfb
bunssss <tkylecaluag26@gmail.com>
2024-03-20 14:35:57 -0700

    HW3

---
+++ Homework 3/C++ Development Root/SourceCode/GroceryItemDatabase.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+///////////////////////// TO-DO (1) //////////////////////////////
+  /// Include necessary header files
+  /// Hint:  Include what you use, use what you include
+  ///
+  /// Do not put anything else in this section, i.e. comments, classes, functions, etc.  Only #include directives
+
+#include "GroceryItem.hpp"
+#include <vector>
+
+/////////////////////// END-TO-DO (1) ////////////////////////////
+
+
+// Singleton Design Pattern
+class GroceryItemDatabase
+{
+  public:
+    // Get a reference to the one and only instance of the database
+    static GroceryItemDatabase & instance();
+
+    // Locate and return a reference to a particular record
+    GroceryItem * find( const std::string & upc );                              // Returns a pointer to the item in the database if
+                                                                                // found, nullptr otherwise
+    // Queries
+    std::size_t size() const;                                                   // Returns the number of items in the database
+
+  private:
+    GroceryItemDatabase            ( const std::string & filename );
+
+    GroceryItemDatabase            ( const GroceryItemDatabase & ) = delete;    // intentionally prohibit making copies
+    GroceryItemDatabase & operator=( const GroceryItemDatabase & ) = delete;    // intentionally prohibit copy assignments
+
+    ///////////////////////// TO-DO (2) //////////////////////////////
+      /// Private implementation details
+      /// Add any necessary private helper functions, member attributes, etc.
+
+    std::size_t              _size = 230;
+    std::vector<GroceryItem> _items;
+    GroceryItem *            find( std::size_t index, const std::string & upc );
+
+    /////////////////////// END-TO-DO (2) ////////////////////////////
+};
